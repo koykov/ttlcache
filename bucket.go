a901958a6d4a1f50dbd714c3f0abe47906664460
Konstantin Voykov <koykov0@gmail.com>
2023-06-27 23:53:05 +0300

    IO operations draft.

--- bucket.go
+++ bucket.go
@@ -7,3 +7,13 @@ type bucket[T any] struct {
 	idx map[uint64]uint
 	buf []entry[T]
 }
+
+func (b *bucket[T]) set(hkey uint64, value T) error {
+	_, _ = hkey, value
+	return nil
+}
+
+func (b *bucket[T]) get(hkey uint64) (T, error) {
+	_ = hkey
+	return any(nil), nil
+}
--- cache.go
+++ cache.go
@@ -6,4 +6,30 @@ type Cache[T any] struct {
 	o       sync.Once
 	conf    *Config
 	buckets []bucket[T]
+
+	err error
+}
+
+func (c *Cache[T]) init() {
+	//
+}
+
+func (c *Cache[T]) Set(key string, value T) error {
+	c.o.Do(c.init)
+	if c.err != nil {
+		return c.err
+	}
+	hkey := c.conf.Hasher.Sum64(key)
+	b := &c.buckets[hkey%uint64(c.conf.Buckets)]
+	return b.set(hkey, value)
+}
+
+func (c *Cache[T]) Get(key string) (T, error) {
+	c.o.Do(c.init)
+	if c.err != nil {
+		return any(nil), c.err
+	}
+	hkey := c.conf.Hasher.Sum64(key)
+	b := &c.buckets[hkey%uint64(c.conf.Buckets)]
+	return b.get(hkey)
 }

c850b3881af041f8207c8581851f0966a6b61770
Konstantin Voykov <koykov0@gmail.com>
2023-07-02 23:24:44 +0300

    Cover config copy with test.

---
+++ config_test.go
@@ -0,0 +1,19 @@
+package ttlcache
+
+import (
+	"testing"
+	"time"
+)
+
+func TestConfig(t *testing.T) {
+	t.Run("copy", func(t *testing.T) {
+		conf := Config{
+			TTLInterval: time.Minute,
+		}
+		cpy := conf.Copy()
+		conf.TTLInterval = 30 * time.Second
+		if cpy.TTLInterval != time.Minute {
+			t.Error("config copy failed")
+		}
+	})
+}

c334d92b84ec3a05e423f76eb0ca354f05cef132
Konstantin Voykov <koykov0@gmail.com>
2024-04-28 22:32:37 +0300

    Extend metrics writer.

--- dummy.go
+++ dummy.go
@@ -6,7 +6,10 @@ type DummyMetrics struct{}
 
 func (DummyMetrics) Set(_ string, _ time.Duration) {}
 func (DummyMetrics) Hit(_ string, _ time.Duration) {}
+func (DummyMetrics) Del(_ string)                  {}
 func (DummyMetrics) Miss(_ string)                 {}
 func (DummyMetrics) Expire(_ string)               {}
 func (DummyMetrics) Overflow(_ string)             {}
 func (DummyMetrics) Evict(_ string)                {}
+func (DummyMetrics) Dump(_ string)                 {}
+func (DummyMetrics) Load(_ string)                 {}
--- metrics.go
+++ metrics.go
@@ -5,8 +5,11 @@ import "time"
 type MetricsWriter interface {
 	Set(bucket string, dur time.Duration)
 	Hit(bucket string, dur time.Duration)
+	Del(bucket string)
 	Miss(bucket string)
 	Expire(bucket string)
 	Overflow(bucket string)
 	Evict(bucket string)
+	Dump(bucket string)
+	Load(bucket string)
 }
